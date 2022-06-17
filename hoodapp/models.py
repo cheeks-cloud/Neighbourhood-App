9b690f45a4634d17de533f12c6fe343ccf95ca6e
rachel <rachelkemuma99@gmail.com>
2022-06-17 23:14:36 +0300

    add a business and do migrations

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200,default='')
+    description = models.TextField(default='')
+
+    def save_business(self):
+        self.save()
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    def __str__(self) -> str:
+        return f'{self.name}Business'
