7cc8868204cced825cbd91d7dded8b7740c55d26
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-17 23:19:38 +0300

    creating the profile model

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE , related_name='profile')
+    bio = models.TextField(max_length=300,blank =True)
+    email = models.CharField(max_length=100, default = '')
+    location = models.CharField(max_length=100,blank =True)
+    profile_pic = models.ImageField( upload_to='profile/', blank ='true',default='default.png')
