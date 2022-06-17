3bb83cee1588c7df9e02beae2ddeaad7340832ab
rachel <rachelkemuma99@gmail.com>
2022-06-17 23:26:05 +0300

    create class post in models and do migrations

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -16,5 +16,21 @@ class Business(models.Model):
     def delete_business(self):
         self.delete()
 
-    def __str__(self) -> str:
-        return f'{self.name}Business'
+    def __str__(self):
+        return f'{self.name}Business' 
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    post = models.TextField()
+    date_posted = models.DateField(auto_now_add=True)
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.title}Post'
+
+
