47a1ca65fefbc86a4409e057877ad41fc98bb770
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-06-18 00:01:39 +0300

    adding a function to create the neighbourhood

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 from .models import *
 
 # Create your views here.
+def create_neighbourhood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            neighbourhood = form.save(commit=False)
+            neighbourhood.admin = request.user
+            neighbourhood.save()
+            messages.success(
+                request, 'You have succesfully created a Neighbourhood.')
+            return redirect('neighbourhood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'new_hood.html', {'form': form})
