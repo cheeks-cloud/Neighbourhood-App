866c4bf3fc37974ae3ce46bbf23fbfce753ea00d
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-18 21:28:23 +0300

    adding logout

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
 from .forms import *
-from django.contrib.auth import login
+from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
 def home(request):
@@ -20,6 +22,31 @@ def register_request(request):
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="register.html", context={"register_form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
+
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.") 
+	return redirect("home")
 
 
 
