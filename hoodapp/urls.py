48d459f7b2671f55f53201ed30c0a80f1bb248d3
oyesa <mercyoyesa2018@gmail.com>
2022-06-18 14:18:35 +0300

    create update profile template

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns = [
-    
-    
+
+urlpatterns = [ 
+  path('', views.home, name='home'),
+  path("profile/", views.profile, name="profile"),
+  path("accounts/profile/", views.profile, name="profile"),
+  path("profile/update/", views.update_profile, name="update_profile"),
 ]
