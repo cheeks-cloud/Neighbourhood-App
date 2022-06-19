ef2961538815417d490dc5ba14be7901dd15da7b
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-19 06:50:35 +0300

    paginating the API view

--- hoodproject/settings.py
+++ hoodproject/settings.py
@@ -142,3 +142,9 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static/') ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
