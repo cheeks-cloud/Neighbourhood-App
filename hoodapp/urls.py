948bff999589a7d837dc5c60de0b6b5d80f9c32f
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-19 18:05:47 +0300

    Add a path for search

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [ 
-  path("profile/", views.profile, name="profile"),
-  path("profile/update/", views.update_profile, name="update_profile"),
-  path('businesses/<id>', views.businesses, name='businesses'),
-  path('new_business/', views.new_business, name='new_business'),
+
+    path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
+    path('profile-update/',views.update_profile, name='update_profile'), 
+    path('profile/<pk>/',views.profile, name = 'profile'),
+    path('create-hood/',views.createhood, name='createhood'), 
+    path('search/',views.search_results, name='search_results'),
+    path('search_hood/',views.search_hoods, name='search_hood'),
 ]
 
 if settings.DEBUG:
