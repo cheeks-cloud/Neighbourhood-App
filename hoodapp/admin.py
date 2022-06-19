b6869fc5de30a26c68f3d0ae4250e84733be2e34
SharonneKemboi <sharon.kemboi01@student.moringaschool.com>
2022-06-19 18:03:04 +0300

    Registerd Post

--- hoodapp/admin.py
+++ hoodapp/admin.py
@@ -4,4 +4,8 @@ from .models import *
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(NeighbourHood)
+admin.site.register(Business)
+admin.site.register(Post)
+
+
 
--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,34 +1,27 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationFor
 from django.contrib.auth.models import User
 from .models import *
 
 class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
-        exclude = ('user',)
+        fields=['image','name','email','phone_no'] 
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ('user',) 
+        fields=['title','info','image']  
 
-class NeighbourHoodForm(forms.ModelForm):
+class HoodForm(forms.ModelForm):
     class Meta:
         model = NeighbourHood
-        exclude = ('admin',)
+        fields=['name','location','description','image','health_center','health_email','health_contact'] 
 
-class ProfileUpdateForm(forms.ModelForm):
-    
+class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio', 'profile_pic', 'location', 'email']
+        fields=['profile_pic','bio','email','phone_no'] 
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
 
-    class Meta:
-        model = User
-        fields = ['username','email']
-        
 
